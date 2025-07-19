f5a65b6f4cee600227cb4a28989fbd6f0dafbbf6
vtarapara <vtara321@gmail.com>
2025-07-19 16:49:35 -0400

    rem error files

--- sll_16_bit.v
+++
@@ -1,7 +0,0 @@
-module sll_16_bit(shifted_out, data_in, shift_amt);
-    output [31:0] shifted_out;
-    input [31:0] data_in;
-    input shift_amt;
-
-    assign shifted_out = shift_amt ? {data_in[15:0], 16'b0000000000000000} : data_in;
-endmodule
--- sra_16_bit.v
+++
@@ -1,7 +0,0 @@
-module sra_16_bit(shifted_out, data_in, shift_amt);
-    output [31:0] shifted_out;
-    input [31:0] data_in;
-    input shift_amt;
-
-    assign shifted_out = shift_amt ? {{16{data_in[31]}}, data_in[31:16]} : data_in;
-endmodule
